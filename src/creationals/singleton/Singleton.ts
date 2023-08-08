864cefbc9f8eee9073a88b4f01b0f9c176df0b5a
Wendell Oliveira <wendelloliveirasud@gmail.com>
2023-08-08 20:32:57 -0300

    finished the Singleton design pattern

--- src/creationals/singleton/Singleton.ts
+++ src/creationals/singleton/Singleton.ts
@@ -1,1 +1,22 @@
-console.log("Hello World, from Singleton...");
+//Logging Service Singleton Class 
+class LoggingServiceSingleton {
+  private static instance: LoggingServiceSingleton;
+
+  private constructor() {}
+
+  public static getInstance(): LoggingServiceSingleton {
+    if (!LoggingServiceSingleton.instance) {
+      LoggingServiceSingleton.instance = new LoggingServiceSingleton();
+    }
+
+    return LoggingServiceSingleton.instance;
+  }
+
+  public log(message: string): void {
+    console.log(message);
+  }
+}
+
+//Client code
+const logger = LoggingServiceSingleton.getInstance();
+logger.log("This is a log message from the singleton logger");
